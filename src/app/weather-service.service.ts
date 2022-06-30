0e48709df8b0e9630ba930f91a7aae03aa878ae3
suraj <suraj.kumar23@gmail.com>
2022-06-30 12:23:21 +0530

    added apikey for weather api

--- src/app/weather-service.service.ts
+++ src/app/weather-service.service.ts
@@ -8,7 +8,7 @@ export class WeatherServiceService {
   url = 'https://api.openweathermap.org/data/2.5/weather'
   constructor(private http:HttpClient) { }
   public getWeatherData(city:string){
-    let queryParam = {q:city,appid:'ff1bc4683fc7325e9c57e586c20cc03e'}
+    let queryParam = {q:city,appid:'2a7bed2ce8c97dcf726ad7aadf5c4097'}
     let query = new HttpParams({fromObject:queryParam})
     return this.http.get(this.url,{params:query})
   }
